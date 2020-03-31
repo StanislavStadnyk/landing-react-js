5f610303150fd4f63193865befaeb5135e5471ed
StanislavStadnik <stanislav.stadnik@dataart.com>
2020-03-31 11:22:33 +0300

    Updates

---
+++ precache-manifest.8ef3a690cf32179c246356a999bcab37.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4aae561b58c9c459e5a4196cbd5fdb9",
+    "url": "/landing-react-js/index.html"
+  },
+  {
+    "revision": "37875ca3eafceab96c83",
+    "url": "/landing-react-js/static/css/2.8ff17cee.chunk.css"
+  },
+  {
+    "revision": "bb69eeb92a844c1960ca",
+    "url": "/landing-react-js/static/css/main.77533a2a.chunk.css"
+  },
+  {
+    "revision": "37875ca3eafceab96c83",
+    "url": "/landing-react-js/static/js/2.8aafc1d0.chunk.js"
+  },
+  {
+    "revision": "96fd13b14354f96e6d9019ca4512b265",
+    "url": "/landing-react-js/static/js/2.8aafc1d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb69eeb92a844c1960ca",
+    "url": "/landing-react-js/static/js/main.83c3bc44.chunk.js"
+  },
+  {
+    "revision": "903e9bfac70aaf4b99b2",
+    "url": "/landing-react-js/static/js/runtime-main.453c73cb.js"
+  },
+  {
+    "revision": "46cea149166ed485bffbd3ff3c0df44f",
+    "url": "/landing-react-js/static/media/icomoon.46cea149.svg"
+  },
+  {
+    "revision": "81d925d5528e1439d5b540dffe96a3fc",
+    "url": "/landing-react-js/static/media/icomoon.81d925d5.woff"
+  },
+  {
+    "revision": "d248802eefb3a3ebecb92be32b937dac",
+    "url": "/landing-react-js/static/media/icomoon.d248802e.eot"
+  },
+  {
+    "revision": "dbc2b7bb35d7c7ebb04388087ae3a70d",
+    "url": "/landing-react-js/static/media/icomoon.dbc2b7bb.ttf"
+  }
+]);
